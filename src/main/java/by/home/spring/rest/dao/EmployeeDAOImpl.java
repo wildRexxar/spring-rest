5b32ab2a124b3a28ae43d6aa308bad09357b4fae
Ilya <ilya.skavysh@gmail.com>
2022-01-04 14:52:48 +0300

    create rest app and add spring configuration

---
+++ src/main/java/by/home/spring/rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,45 @@
+package by.home.spring.rest.dao;
+
+import by.home.spring.rest.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        return session
+                .createQuery("from Employee", Employee.class)
+                .getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        int query = session
+                .createQuery("delete from Employee where id =:id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+}
